950ab049532de403cbf695a7e0235301a58a2edb
Rodrigo Silvestre <rodrigo291002@hotmail.com>
2026-04-23 18:53:33 -0300

    Template

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,5 +1,5 @@
-from django.http import HttpResponse
+from django.shortcuts import render
 
 
 def post_view(request):
-    return HttpResponse("Hello World")
+    return render(request, "blog/index.html")
--- mysite/tests/views/test_post_view.py
+++ mysite/tests/views/test_post_view.py
@@ -2,4 +2,6 @@ def test_post_view_returns_hello_world(client):
     response = client.get("/post/")
 
     assert response.status_code == 200
-    assert response.content == b"Hello World"
+    assert b"Hello World" in response.content
+    template_names = [template.name for template in response.templates if template.name]
+    assert "blog/index.html" in template_names
