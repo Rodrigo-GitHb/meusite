56e73a627ce069705ca7949832832723a0773dda
Rodrigo Silvestre <rodrigo291002@hotmail.com>
2026-04-27 17:25:36 -0300

    Fix Django admin add page

--- mysite/blog/admin.py
+++ mysite/blog/admin.py
@@ -8,4 +8,3 @@ class PostAdmin(admin.ModelAdmin):
     list_display = ("title", "slug", "status", "created_on")
     search_fields = ("title", "content")
     list_filter = ("status", "created_on")
-    prepopulated_fields = {"slug": ("title",)}
